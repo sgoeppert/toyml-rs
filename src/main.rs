9c35d676091c95a78c1188de9efe1f23100cb7a2
Stefan Göppert <stefan.goeppert@gmail.com>
2022-05-18 20:40:13 +0200

    Implement basic matrix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+
+#[derive(Debug, Clone)]
+struct Dim {
+    shape: Vec<usize>
+}
+
+impl Dim {
+    fn new(shape: &[usize]) -> Self {
+        assert!(shape.len() > 1, "The shape must have at least 2 dimensions. For a column vector use [N, 1] and for a row vector [1, N]");
+        Dim {
+            shape: shape.to_vec()
+        }
+    }
+
+    fn total_elements(&self) -> usize {
+        self.shape.iter().product()
+    }
+}
+
+#[derive(Debug)]
+struct Matrix {
+    data: Vec<f64>,
+    shape: Dim
+}
+
+impl Matrix {
+    fn new(data: &[f64], shape: &[usize]) -> Self {
+        let shape = Dim::new(shape);
+        assert_eq!(shape.total_elements(), data.len(), "The data does not fit the shape");
+        Matrix {
+            data: data.to_vec(),
+            shape
+        }
+    }
+
+    fn dim(&self) -> Dim {
+        self.shape.clone()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m = Matrix::new(&[1., 2., 3., 4., 5., 6.,], &[1, 6]);
+
+    println!("{:?}", m);
 }
